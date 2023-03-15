f274ac62a9fca93e8397a17ceef1032943125a17
Javier Arguello <arguello@galatea-associates.com>
2023-03-15 13:58:03 -0400

    Updated hello again

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    @GetMapping("/hello")
+    @GetMapping("/helloworld")
     public String hello() {
         return "Hello world!";
     }
